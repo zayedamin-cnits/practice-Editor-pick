65516be46f971e06e297b6a87a9ea6e5ac52df7b
zayed1611 <zayedrohan.cnits@gmail.com>
2024-02-15 19:07:43 +0600

    3th commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+
+$("#slider").owlCarousel({
+    loop: true,
+    margin: 15,
+    nav: true,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+  
