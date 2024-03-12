074883c2190e9efa7fe9a67b5c836e1bf8fe3285
zayedamin-cnits <zayedrohan.cnits@gmail.com>
2024-03-12 16:25:58 +0600

    11th commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
 
 
 
-$("#slider").owlCarousel({
-    loop: true,
-    margin: 15,
-    nav: true,
-    autoplay: true,
- 
-    dots:false,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 2,
-      },
-      1000: {
-        items: 5,
-      },
+var swiper = new Swiper(".card_slider", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    225: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-  });
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    878: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
+});
   
 
 
