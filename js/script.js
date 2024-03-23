452ead36f8c1901254861d8f42df6fbb5f8eb2ad
zayedamin-cnits <zayedrohan.cnits@gmail.com>
2024-03-23 15:58:38 +0600

    13th commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
 
-
 var swiper = new Swiper(".card_slider", {
   slidesPerView: 4,
   spaceBetween: 20,
@@ -45,3 +44,43 @@ var swiper = new Swiper(".card_slider", {
 
 
 
+var swiper = new Swiper(".card_slider2", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    225: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    878: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+});
